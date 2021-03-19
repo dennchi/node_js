b73bf562e1dceeb48e7e423b5b393ad0a35b5a0d
dennchi <bosatsu2211@gmail.com>
2021-03-20 03:13:44 +0900

    practice

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models/index');
 
+const { Op } = require('sequelize');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   db.User.findAll().then(usrs => {
@@ -11,6 +13,26 @@ router.get('/', function(req, res, next) {
     }
     res.render('users/index', data);
   });
+});
+
+router.get('/add', (req, res, next)=> {
+  var data = {
+    title: 'Users/Add'
+  }
+  res.render('users/add', data);
+});
+
+router.post('/add', (req, res, next)=> {
+  db.sequelize.sync()
+    .then(() => db.User.create({
+      name: req.body.name,
+      pass: req.body.pass,
+      mail: req.body.mail,
+      age: req.body.age
+    }))
+    .then(usr => {
+      res.redirect('/users');
+    });
 });
 
 module.exports = router;
